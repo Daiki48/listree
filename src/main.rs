ba8aeeb1ca79368bfacc064e7452967dbbaff26e
Daiki48 <daiki48.engineer@gmail.com>
2022-10-10 17:10:00 +0900

    [update] Prepare source code

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,31 @@ use std::fs;
 
 fn main() {
     let target = "./";
-    let mut files: Vec<String> = Vec::new();
+    let mut files: Vec<String> = Vec::new();    // init Vec
+
+    // Store in vec
     for path in fs::read_dir(target).unwrap(){
-        files.push(path.unwrap().path().display().to_string().replacen(target, "", 1))
+        files.push(
+            path.unwrap()
+                .path()
+                .display()
+                .to_string()
+                .replacen(target, "", 1)
+        )
     }
+
+    // sorting
     files.sort();
+
+    // Converts from vec to String
     let strings = files.iter()
         .fold(String::new(), |joined, s| {
-            if joined == String::new() { s.to_string() } else { joined + " " + s }
+            if joined == String::new() {
+                s.to_string()
+            } else {
+                joined + " " + s
+            }
         });
-
     println!("{}", strings);
+
 }
