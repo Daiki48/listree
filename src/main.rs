f45a741be3905cee90313ef9470661f35063cc69
Daiki <102207294+Daiki48@users.noreply.github.com>
2023-06-08 21:47:16 +0900

    :wrench: Added current_path

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
+use std::env;
 use std::fs;
+use std::io;
 
-fn main() {
+fn main() -> io::Result<()> {
+    let current_path = env::current_dir()?;
+    println!("{}", current_path.display());
     let target = "./";
-    let mut files: Vec<String> = Vec::new();    // init Vec
+    let mut files: Vec<String> = Vec::new(); // init Vec
 
     // Store in vec
-    for path in fs::read_dir(target).unwrap(){
+    for path in fs::read_dir(target).unwrap() {
         files.push(
             path.unwrap()
                 .path()
                 .display()
                 .to_string()
-                .replacen(target, "", 1)
+                .replacen(target, "", 1),
         )
     }
 
@@ -19,14 +23,13 @@ fn main() {
     files.sort();
 
     // Converts from vec to String
-    let strings = files.iter()
-        .fold(String::new(), |joined, s| {
-            if joined == String::new() {
-                s.to_string()
-            } else {
-                joined + "\n" + s
-            }
-        });
+    let strings = files.iter().fold(String::new(), |joined, s| {
+        if joined == String::new() {
+            s.to_string()
+        } else {
+            joined + "\n" + s
+        }
+    });
     println!("{}", strings);
-
+    Ok(())
 }
